cac96fe798ca36534e81fe859de3f2766780da29
KoteAnimen <sverh.il@yandex.ru>
2020-11-17 10:41:14 +0300

    доработать структуру Person

--- pract-9/Form1.cs
+++ pract-9/Form1.cs
@@ -10,6 +10,32 @@ using System.Windows.Forms;
 
 namespace pract_9
 {
+
+    struct Person
+    {
+        string firstName;
+        string secondName;
+        string middleName;
+        bool genderMale;
+        bool genderFemale;
+        int yearBorn;
+        string placeBorn;
+        string nationality;
+
+        public Person(string namePerson, string surname, string middleNamePerson, bool male, bool female, int year, string place, string nation)
+        {
+            firstName = namePerson;
+            secondName = surname;
+            middleName = middleNamePerson;
+            genderMale = male;
+            genderFemale = female;
+            yearBorn = year;
+            placeBorn = place;
+            nationality = nation;
+        }
+       
+    }
+
     public partial class Form1 : Form
     {
         public Form1()
